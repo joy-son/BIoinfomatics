3f62b9ac78b6f015d181362a2737f79f57b85a93
joyTooday <63324419+joy-son@users.noreply.github.com>
2021-11-03 05:03:06 +0900

    frequency simplify

--- Hamming distance/hd.py
+++ Hamming distance/hd.py
@@ -7,6 +7,7 @@ fw_dt = open("./HammingDistance.txt","w")
 fw_fq = open("./HammingFrequency.txt","w")
 
 l_indexset = []
+l_fq = []
 d_fq = {}
 
 fw_dt.write("index_set") #index sets 생성
@@ -19,22 +20,16 @@ for line in lines:
 for row in l_indexset:
     fw_dt.write(f"\n{row}") #distance file row 생성
     i_row = [list(row.split("+")[0]), list(row.split("+")[1])]
-    for col in l_indexset: #distance result 생성
+    for col in l_indexset: 
         i_col = [list(col.split("+")[0]), list(col.split("+")[1])]
-        fw_dt.write(f"\t={int(distance.hamming(i_row[0],i_col[0])*len(i_row[0]))}+{int(distance.hamming(i_row[1],i_col[1])*len(i_row[1]))}")
-        d_fq[
-            f"{int(distance.hamming(i_row[0],i_col[0])*len(i_row[0]))}+{int(distance.hamming(i_row[1],i_col[1])*len(i_row[1]))}"
-        ] = (
-            d_fq.get(
-                f"{int(distance.hamming(i_row[0],i_col[0])*len(i_row[0]))}+{int(distance.hamming(i_row[1],i_col[1])*len(i_row[1]))}",
-                0,
-            )
-            + 1
-        ) #frequency {}에 저장
+        fw_dt.write(f"\t={int(distance.hamming(i_row[0],i_col[0])*len(i_row[0]))}+{int(distance.hamming(i_row[1],i_col[1])*len(i_row[1]))}") #distance result 생성
+        l_fq.append(f"{int(distance.hamming(i_row[0], i_col[0]) * len(i_row[0]))} + {int(distance.hamming(i_row[1], i_col[1]) * len(i_row[1]))}") #frequency []
 
+for i in l_fq:
+    d_fq[i] = d_fq.get(i, 0) + 1  # frequency {} 생성        
 fw_fq.write("HammingDist_i7\tHammingDist_i5\tIndex_Count") #frequency file column 생성
 for key in d_fq:
-    fw_fq.write(f"\n{key.split('+')[0]}\t{key.split('+')[1]}\t{d_fq[key]}")
+    fw_fq.write(f"\n{key.split('+')[0]}\t{key.split('+')[1]}\t{d_fq[key]}") #frequency result 생성
 
 fr.close()
 fw_dt.close()
